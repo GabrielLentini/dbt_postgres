75ce0d0f3e1f6da38418d5616bbbf3c3c9ee5a3b
Gabriel Lentini <gabriel.marques@sgaseguros.com.br>
2025-11-16 20:43:02 -0300

    develop - Foi criada a dim_equipe e suas tabelas intermediárias

---
+++ models/marts/dim_equipes.sql
@@ -0,0 +1,26 @@
+{{ config(
+    unique_key='id_dim_pilotos',
+    materialized='table',
+    tags=['pilotos']
+) }}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['de.id_equipe']) }} AS id_dim_equipes,
+    de.id_equipe,
+    c.nome_equipe,
+    de.pontos_totais_equipe,
+    COALESCE(te.titulos, 0) AS titulos,
+    de.vitorias,
+    de.podios,
+    de.pole_positions,
+    de.largadas_top10,
+    de.largadas_fora_top10,
+    c.nacionalidade_equipe,
+    pe.pilotos_da_equipe
+FROM {{ ref('int_dados_equipes') }} AS de
+LEFT JOIN {{ ref('int_titulos_equipes') }} AS te
+    ON te.id_equipe = de.id_equipe
+LEFT JOIN {{ ref('stg_construtores') }} AS c
+    ON c.id_equipe = de.id_equipe
+LEFT JOIN {{ ref('int_pilotos_equipe') }} AS pe
+    ON pe.id_equipe = de.id_equipe
--- models/staging/stg_pilotos.sql
+++ models/staging/stg_pilotos.sql
@@ -10,6 +10,7 @@ SELECT
     {{ replace_diacritics("code") }} AS abreviacao_piloto,
     {{ replace_diacritics("forename") }} AS primeiro_nome_piloto,
     {{ replace_diacritics("surname") }} AS sobrenome_piloto,
+    CONCAT({{ replace_diacritics("forename") }}, ' ', {{ replace_diacritics("surname") }}) AS nome_piloto,
     dob AS data_nascimento_piloto,
     {{ replace_diacritics("nationality") }} AS nacionalidade_piloto,
     url AS wikipedia_piloto,
