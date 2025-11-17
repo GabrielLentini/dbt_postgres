eb969bedbc6d090056fca6229c10f40c551b33f7
Gabriel Lentini <gabriel.marques@sgaseguros.com.br>
2025-11-16 23:17:10 -0300

    develop - Foi criada a dim_estatisticas e todas suas tabelas int

---
+++ models/marts/dim_estatisticas.sql
@@ -0,0 +1,21 @@
+{{ config(
+    unique_key='id_estatisticas',
+    materialized='table',
+    tags=['status', 'estatisticas']
+) }}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['pc.id_piloto', 'pt.numero_paradas', 'pc.ano_campeonato']) }} AS id_estatisticas,
+    pc.id_piloto,
+    pc.ano_campeonato AS ano,
+    COALESCE(pt.numero_paradas::text, '(N/A)') AS numero_paradas,
+    pt.numero_paradas AS numero_paradas_int,
+    st.status_piloto_ano,
+    pc.colocacao_no_campeonato
+FROM {{ ref('int_posicao_campeonato') }} AS pc
+LEFT JOIN {{ ref('int_status') }} AS st
+    ON pc.id_piloto = st.id_piloto
+    AND pc.ano_campeonato = st.ano_corrida
+LEFT JOIN {{ ref('int_pit_stops') }} AS pt
+    ON pt.id_piloto = pc.id_piloto
+    AND pt.ano_corrida = pc.ano_campeonato
