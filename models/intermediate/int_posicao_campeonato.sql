31417bd3144c720fbee3eaa9467cf36563e18784
Gabriel Lentini <gabriel.marques@sgaseguros.com.br>
2025-11-21 15:32:16 -0300

    Foram feitas documentações e arrumado código da int_posicao_campeonato agora abrangindo também modelo de pontuação antigo

--- models/intermediate/int_posicao_campeonato.sql
+++ models/intermediate/int_posicao_campeonato.sql
@@ -4,27 +4,41 @@
     tags=['pilotos']
 ) }}
 
-WITH pontos_por_piloto AS (
+WITH pontos_corrida AS (
     SELECT
         r.id_piloto,
         c.ano_corrida,
-        SUM((COALESCE(r.pontos, 0) + COALESCE(s.pontos, 0))) AS pontos_no_ano
-    FROM {{ ref('stg_resultados') }} AS r
-    LEFT JOIN {{ ref('stg_corridas') }} AS c
-        ON r.id_corrida = c.id_corrida
-    LEFT JOIN {{ ref('stg_sprint') }} AS s
-        ON
-            r.id_corrida = s.id_corrida
-            AND r.id_piloto = s.id_piloto
-    GROUP BY r.id_piloto, c.ano_corrida
+        (COALESCE(r.pontos, 0) + COALESCE(s.pontos, 0)) AS pontos_corrida,
+        ROW_NUMBER() OVER (
+            PARTITION BY r.id_piloto, c.ano_corrida
+            ORDER BY (COALESCE(r.pontos, 0) + COALESCE(s.pontos, 0)) DESC
+        ) AS rn
+    FROM {{ ref('stg_resultados') }} r
+    LEFT JOIN {{ ref('stg_corridas') }} c ON r.id_corrida = c.id_corrida
+    LEFT JOIN {{ ref('stg_sprint') }} s
+        ON r.id_corrida = s.id_corrida
+        AND r.id_piloto = s.id_piloto
+),
+
+pontos_validos AS (
+    SELECT
+        id_piloto,
+        ano_corrida,
+        SUM(pontos_corrida) AS pontos_no_ano
+    FROM pontos_corrida
+    WHERE
+        (ano_corrida <= 1990 AND rn <= 11)
+        OR
+        (ano_corrida > 1990)
+    GROUP BY id_piloto, ano_corrida
 )
 
 SELECT
     id_piloto,
-    ano_corrida AS ano_campeonato,
+    ano_corrida AS ano,
     pontos_no_ano,
     ROW_NUMBER() OVER (
         PARTITION BY ano_corrida
         ORDER BY pontos_no_ano DESC
     ) AS colocacao_no_campeonato
-FROM pontos_por_piloto
+FROM pontos_validos
--- models/marts/dim_estatisticas.sql
+++ models/marts/dim_estatisticas.sql
@@ -6,9 +6,9 @@
 
 --noqa:disable=LT05, CV11
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['pc.id_piloto', 'pt.numero_paradas', 'pc.ano_campeonato']) }} AS id_dim_estatisticas,
+    {{ dbt_utils.generate_surrogate_key(['pc.id_piloto', 'pt.numero_paradas', 'pc.ano']) }} AS id_dim_estatisticas,
     pc.id_piloto,
-    pc.ano_campeonato AS ano,
+    st.ano,
     COALESCE(pt.numero_paradas::text, '(N/A)') AS numero_paradas,
     pt.numero_paradas AS numero_paradas_int,
     st.status_piloto_ano,
@@ -17,8 +17,8 @@ FROM {{ ref('int_posicao_campeonato') }} AS pc
 LEFT JOIN {{ ref('int_status') }} AS st
     ON
         pc.id_piloto = st.id_piloto
-        AND pc.ano_campeonato = st.ano_corrida
+        AND pc.ano = st.ano
 LEFT JOIN {{ ref('int_pit_stops') }} AS pt
     ON
         pt.id_piloto = pc.id_piloto
-        AND pt.ano = pc.ano_campeonato
+        AND pt.ano = pc.ano
